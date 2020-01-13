c869e95ce4169c04e260cb76cae34fc03eeea6f7
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-01-13 15:37:45 +1100

    adding teleop drive
    some errors need to comple with gradlew

--- robotcode/2020code/src/main/java/frc/robot/Robot.java
+++ robotcode/2020code/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Controller;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,6 +26,20 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
+
+
+  /**
+   * this is setting up the motor objects so we can use motors as called by 
+   * left motor x and right motor x
+   * the (number here) is the pwn port the motor is on
+   **/
+  private Spark leftMotor1 = new Spark(0);
+  private Spark leftMotor2 = new Spark(1);
+  private Spark rightMotor1 = new Spark(2);
+  private Spark rightMotor2 = new Spark(3);
+
+  private XboxController controller = new XboxController(0); 
+
   @Override
   public void robotInit() {
   }
@@ -39,6 +58,17 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    double speed = -controller.getX(Hand.kLeft);
+    double turn = controller.getY(Hand.kLeft);
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    leftMotor1.set(left);
+    leftMotor2.set(left);
+
+    rightMotor1.set(-right);
+    rightMotor2.set(-right);
   }
 
   @Override
